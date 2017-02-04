25ed51b841b43f1259d3f996e1411c30c86be067
al6obasi <maltobasi@hotmail.com>
2017-02-04 09:50:15 +0200

    sub sol 4

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -17,7 +17,16 @@ angular.module('app', ['ngRoute'])
 
 /*  HINT: Make sure your controllers, methods, and variables 
     are named what $routeProvider and the partials are expecting  */
-.factory('counter', function(){})
+.factory('counter', function(){
+  // counter.count = 0;
+  // return {
+  //   increment:function(){
+      // counter.count ++ ;
+  //   }
+  // }
+  // I forget how can use the variables and method  in each controller
+  // so i make it as show  
+})
 .controller('fizzbuzzCtrl', function($scope){
   var count =0;
   $scope.display = count
@@ -29,7 +38,7 @@ angular.module('app', ['ngRoute'])
     }else if(count % 3 === 0){
       $scope.display = "FIZZ"
     }else if(count % 5=== 0){
-      $scope.display = "Buzz"
+      $scope.display = "BUZZ"
    }else{
     $scope.display = count
    }
@@ -41,12 +50,12 @@ angular.module('app', ['ngRoute'])
   $scope.increment =function(){
     count++;
     $scope.display = count
-    if(count % 3 === 0  && count % 5 ===0 ){
-      $scope.display = "FIZZBUZZ"
-    }else if(count % 3 === 0){
-      $scope.display = "FIZZ"
-    }else if(count % 5=== 0){
-      $scope.display = "Buzz"
+    if(count % 4 === 0  && count % 6 ===0 ){
+      $scope.display = "FOZZBAZZ"
+    }else if(count % 4 === 0){
+      $scope.display = "FOZZ"
+    }else if(count % 6=== 0){
+      $scope.display = "BAZZ"
    }else{
     $scope.display = count
    }
