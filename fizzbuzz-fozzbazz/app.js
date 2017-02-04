d339ba4833f69c643ba97a5988c0cc623be2ef09
al6obasi <maltobasi@hotmail.com>
2017-02-04 09:41:02 +0200

    sol sub 3

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -17,10 +17,38 @@ angular.module('app', ['ngRoute'])
 
 /*  HINT: Make sure your controllers, methods, and variables 
     are named what $routeProvider and the partials are expecting  */
-
-  .factory('counter', function(){})
-  .controller('fizzbuzzCtrl', function($scope){
-    $scope.display ='Moh';
-    console.log($scope.display)
-  })
-  .controller('fozzbazzCtrl', function(){});
+.factory('counter', function(){})
+.controller('fizzbuzzCtrl', function($scope){
+  var count =0;
+  $scope.display = count
+  $scope.increment =function(){
+    count++;
+    $scope.display = count
+    if(count % 3 === 0  && count % 5 ===0 ){
+      $scope.display = "FIZZBUZZ"
+    }else if(count % 3 === 0){
+      $scope.display = "FIZZ"
+    }else if(count % 5=== 0){
+      $scope.display = "Buzz"
+   }else{
+    $scope.display = count
+   }
+ }
+})
+.controller('fozzbazzCtrl', function($scope){
+  var count =0;
+  $scope.display = count
+  $scope.increment =function(){
+    count++;
+    $scope.display = count
+    if(count % 3 === 0  && count % 5 ===0 ){
+      $scope.display = "FIZZBUZZ"
+    }else if(count % 3 === 0){
+      $scope.display = "FIZZ"
+    }else if(count % 5=== 0){
+      $scope.display = "Buzz"
+   }else{
+    $scope.display = count
+   }
+ }
+});
